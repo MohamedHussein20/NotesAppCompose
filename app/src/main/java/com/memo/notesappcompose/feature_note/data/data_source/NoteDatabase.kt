39abd4339bed210acfd5ac486c531056dd093f9c
Mohamed Hussein <mohussein1279@gmail.com>
2025-06-04 00:02:39 +0300

    setup dagger hilt

--- app/src/main/java/com/memo/notesappcompose/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/memo/notesappcompose/feature_note/data/data_source/NoteDatabase.kt
@@ -10,4 +10,7 @@ import com.memo.notesappcompose.feature_note.domain.model.Note
 )
 abstract class NoteDatabase : RoomDatabase() {
     abstract val noteDao: NoteDao
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
